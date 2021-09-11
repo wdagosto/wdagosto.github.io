1a9bb22c883aa3d39628978b448d822ed7b0499b
Aaron Carneiro <euaaron@outlook.com>
2021-09-11 12:24:50 -0300

    fix: dark-mode issues at timeline

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,7 @@ var page = document.body;
 var theme = "light";
 
 $(function() {
-    $(".tl-timenav").css({
-        "border": "none"
-    });
+    $(".tl-timenav").css({ "border": "none" });
 
     $("#nightmode").click(function () {
 
@@ -14,29 +12,23 @@ $(function() {
             $(page).css("background-color", "black");
             $(".tl-text-content-container .tl-headline").css("color", "white");
             $(".tl-slider-container-mask").css("background-color", "black");
-            $(".tl-timemarker-content-container").css({ "background-color" : "#5e5e5e", "border" : "black"});
-            $(".tl-timenav").css({
-                "background-color" : "#2d2d2d"
-            });
-            $(".tl-timeaxis").css({
-                "background-color" : "#000"
-            });
-
+            $(".tl-timemarker-content-container").css({ "backgroundColor" : "#5e5e5e", "border" : "black"});
+            $(".tl-timenav").css({ "backgroundColor" : "#2d2d2d" });
+            $(".tl-timeaxis-background").css("background-color", "black");
+            $(".tl-attribution").css("background-color", "black");
+            $(".tl-timeaxis").css({ "backgroundColor" : "#000" });
+            $(".tl-timeaxis").css({ "backgroundColor" : "#000" });
             theme = "dark";
         } else {
             //Background color
             $(page).css("background-color", "white");
             $(".tl-text-content-container .tl-headline").css("color", "black");
             $(".tl-slider-container-mask").css("background-color", "white");
-            $(".tl-timemarker-content-container").css({
-                "background-color" : "#e5e5e5"
-            });
-            $(".tl-timenav").css({
-                "background-color" : "#f2f2f2"
-            });
-            $(".tl-timeaxis").css({
-                "background-color" : "white"
-            });
+            $(".tl-timemarker-content-container").css({ "backgroundColor" : "#e5e5e5" });
+            $(".tl-timenav").css({ "backgroundColor" : "#f2f2f2" });
+            $(".tl-timeaxis-background").css("background-color", "white");
+            $(".tl-attribution").css("background-color", "white");
+            $(".tl-timeaxis").css({ "backgroundColor" : "white" });
 
             theme = "light";
         }
